5f8427fc20de300bd576a9f140a8c24ec04f0ca4
Matheus Proenca <matheusproencaz@gmail.com>
2023-07-21 13:02:39 -0300

    Correção dto

--- internal/infra/kafka/consumer.go
+++ internal/infra/kafka/consumer.go
@@ -1,8 +1,6 @@
 package kafka
 
-import (
-	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
-)
+import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 
 type Consumer struct {
 	ConfigMap *ckafka.ConfigMap
--- internal/market/dto/dto.go
+++ internal/market/dto/dto.go
@@ -4,10 +4,10 @@ type TradeInput struct {
 	OrderID       string  `json:"order_id"`
 	InvestorID    string  `json:"investor_id"`
 	AssetID       string  `json:"asset_id"`
-	CurrentShares int     `json:"currentShares"`
+	CurrentShares int     `json:"current_shares"`
 	Shares        int     `json:"shares"`
-	Price         float64 `json:"price_id"`
-	OrderType     string  `json:"orderType_id"`
+	Price         float64 `json:"price"`
+	OrderType     string  `json:"order_type"`
 }
 
 type OrderOutput struct {
